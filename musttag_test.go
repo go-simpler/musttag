6f7bf03f16ba5e106c980dd25fff32cf81cc08d2
Tom <github@junk1tm.me>
2022-11-25 16:50:55 +0400

    test: add examples (#2)

--- musttag_test.go
+++ musttag_test.go
@@ -3,6 +3,7 @@ package musttag
 import (
 	"go/ast"
 	"go/token"
+	"path/filepath"
 	"strings"
 	"testing"
 
@@ -11,16 +12,23 @@ import (
 	"golang.org/x/tools/go/types/typeutil"
 )
 
-func Test(t *testing.T) {
-	// for the tests we want to record reports from all functions.
-	reportOnce = false
-	reportf = func(pass *analysis.Pass, call *ast.CallExpr, pos token.Pos, tag string) {
-		fn := typeutil.StaticCallee(pass.TypesInfo, call)
-		name := fn.FullName()
-		pass.Reportf(pos, shortName(name))
-	}
-	testdata := analysistest.TestData()
-	analysistest.Run(t, testdata, Analyzer)
+// TODO(junk1tm): do not depend on tests execution order
+func TestAll(t *testing.T) {
+	t.Run("examples", func(t *testing.T) {
+		dir := filepath.Join(analysistest.TestData(), "examples")
+		analysistest.Run(t, dir, Analyzer)
+	})
+
+	t.Run("tests", func(t *testing.T) {
+		// for the tests we want to record reports from all functions.
+		reportOnce = false
+		reportf = func(pass *analysis.Pass, call *ast.CallExpr, pos token.Pos, tag string) {
+			fn := typeutil.StaticCallee(pass.TypesInfo, call)
+			pass.Reportf(pos, shortName(fn.FullName()))
+		}
+		dir := filepath.Join(analysistest.TestData(), "tests")
+		analysistest.Run(t, dir, Analyzer)
+	})
 }
 
 func shortName(name string) string {
