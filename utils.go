6e7a680087185835a87c6e03b19d0c5fcd792e47
Tom <73077675+tmzane@users.noreply.github.com>
2023-08-16 19:08:49 +0300

    add tests

--- utils.go
+++ utils.go
@@ -1,7 +1,6 @@
 package musttag
 
 import (
-	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -11,32 +10,40 @@ import (
 	"strings"
 )
 
+var (
+	getwd = os.Getwd
+
+	commandOutput = func(name string, args ...string) (string, error) {
+		output, err := exec.Command(name, args...).Output()
+		return string(output), err
+	}
+)
+
 func getMainModule() (string, error) {
 	args := []string{"go", "list", "-m", "-json"}
 
-	data, err := exec.Command(args[0], args[1:]...).Output()
+	output, err := commandOutput(args[0], args[1:]...)
 	if err != nil {
 		return "", fmt.Errorf("running `%s`: %w", strings.Join(args, " "), err)
 	}
 
-	var module struct {
-		Path      string `json:"Path"`
-		Main      bool   `json:"Main"`
-		Dir       string `json:"Dir"`
-		GoMod     string `json:"GoMod"`
-		GoVersion string `json:"GoVersion"`
-	}
-
-	cwd, _ := os.Getwd()
-	decoder := json.NewDecoder(bytes.NewBuffer(data))
+	cwd, _ := getwd()
+	decoder := json.NewDecoder(strings.NewReader(output))
 
 	for {
+		// multiple JSON objects will be returned when using Go workspaces; see #63 for details.
+		var module struct {
+			Path      string `json:"Path"`
+			Main      bool   `json:"Main"`
+			Dir       string `json:"Dir"`
+			GoMod     string `json:"GoMod"`
+			GoVersion string `json:"GoVersion"`
+		}
 		if err := decoder.Decode(&module); err != nil {
 			if errors.Is(err, io.EOF) {
-				return "", fmt.Errorf("no main module in: %s", string(data))
+				return "", fmt.Errorf("main module not found\n%s", output)
 			}
-
-			return "", fmt.Errorf("decoding json: %w: %s", err, string(data))
+			return "", fmt.Errorf("decoding json: %w\n%s", err, output)
 		}
 
 		if module.Main && strings.HasPrefix(cwd, module.Dir) {
